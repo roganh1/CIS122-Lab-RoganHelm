1d57ff812d44693f407cffe14e53ca23fc57149c
Rogan Helm <rogan.helm@mnsu.edu>
2022-11-20 13:57:05 -0600

    Song Lyrics Project

---
+++ Lyrics Project/Program.cs
@@ -0,0 +1,12 @@
+﻿// Written by Rogan Helm
+// 11/14/22
+namespace Lyrics_Project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Lyrics Project/SongLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace Lyrics_Project
+{
+    public class SongLibrary
+    {
+        private List<string> songs = new List<string>();
+        private SpeechSythesis 
+    }
+}
